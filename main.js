8fcd38bb9b74dc3b609dc3f62295afda4d7c7230
Enzo Soliz <enzosoliz95@gmail.com>
2023-01-16 21:49:30 -0300

    Agregando el carrito de compras al index, y aplicando JS, con el curso practico de JS

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu(){
-desktopMenu.classList.toggle('inactive');
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');       
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+    
+    mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isdesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    if (!isdesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 }
